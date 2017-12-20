93fef211c794386332dc67fa420e5e7449df8500
hrayr127981 <hrayr281174@mail.ru>
2017-12-20 18:09:05 +0400

    2 rd taks

---
+++ Check Permutation/statement.js
@@ -0,0 +1,5 @@
+/**
+ * Created by hrayr on 12/20/2017.
+ */
+Given two strings,write a method to decide if one is a permutation of the
+other.
---
+++ IsUniqueCharachters/statement.js
@@ -0,0 +1,7 @@
+/**
+ * Created by hrayr on 12/20/2017.
+ */
+function isUnique(str) {
+    // return true if all characters are unique
+    // else false
+}
